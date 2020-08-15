27bc1ebb3d84da29ea2ee3f6753ccd9b6bfb7fa6
unknown <tariq047@gmail.com>
2020-08-15 19:13:57 +0100

    combined separate project files with the index file

---
+++ assets/js/flack.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    listAnimationOnScroll();
+});
+
+function listAnimationOnScroll () {
+    const observerOptions = {
+        root: document.querySelector('#scrollArea'),
+        rootMargin: '0px',
+        threshold: 0.9 //fraction of target that is visible
+    }
+    
+    let listAnimationObserver = new IntersectionObserver(listAnimation, observerOptions);
+    let animationObserverTarget = document.querySelector('ul.animate');
+    listAnimationObserver.observe(animationObserverTarget);
+}
+
+function listAnimation(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log("intersecting")
+            entry.target.classList.add('start');
+            observer.disconnect();
+        }
+    });
+}
