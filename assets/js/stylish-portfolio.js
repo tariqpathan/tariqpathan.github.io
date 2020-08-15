01c19aff736c76929e1e7078f79050d81d94f9f6
unknown <tariq047@gmail.com>
2020-08-15 17:04:54 +0100

    first commit

---
+++ assets/js/stylish-portfolio.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  //   debugger;
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000, "easeInOutExpo");
+  //       return false;
+  //     }
+  //   }
+  // });
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($('#mainNav').offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+  
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+}); // End of use strict
+
+// sidebar related
+// // Closes the sidebar menu
+  // $(".menu-toggle").click(function(e) {
+  //   e.preventDefault();
+  //   $("#sidebar-wrapper").toggleClass("active");
+  //   $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  //   $(this).toggleClass("active");
+  // });
+
+  // // Closes responsive menu when a scroll trigger link is clicked
+  // $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+  //   $("#sidebar-wrapper").removeClass("active");
+  //   $(".menu-toggle").removeClass("active");
+  //   $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  // });
