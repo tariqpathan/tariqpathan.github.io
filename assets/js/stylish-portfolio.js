e7e539f957943b96baced8f2eee47a04e360b80a
unknown <tariq047@gmail.com>
2020-09-02 20:12:46 +0100

    split main page into several pages

--- assets/js/stylish-portfolio.js
+++ assets/js/stylish-portfolio.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
   "use strict"; // Start of use strict
   // Creates an image slider
-  var slider = new BeerSlider(document.getElementById('image-slider-container'));
+  //var slider = new BeerSlider(document.getElementById('image-slider-container'));
 
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+    console.log(this.hostname);  
+    var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      console.log(target, target.length);
-      console.log(target.offset().top - 50);
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 50)
@@ -19,30 +18,11 @@ $(document).ready(function() {
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').not('#navbarDropdown').click(function() {
-    $('.navbar-collapse').collapse('hide');
-    setTimeout(() => $('div.dropdown-menu').removeClass("show"), 400);
-  });
+  // $('.js-scroll-trigger').not('#navbarDropdown').click(function() {
+  //   $('.navbar-collapse').collapse('hide');
+  //   setTimeout(() => $('div.dropdown-menu').removeClass("show"), 400);
+  // });
 
-  // // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($('#mainNav').offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-  
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
